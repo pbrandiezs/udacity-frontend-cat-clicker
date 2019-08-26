34c57a888b98d542996f934aafed0eac194eef7d
Perry Brandiezs <pbrandiezs@gmail.com>
2019-08-26 15:07:53 -0700

    feat: Created cat object and aside list

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-var click_count0 = 0;
-var click_count1 = 0;
-var elem0 = document.getElementById('cat0');
-var elem1 = document.getElementById('cat1');
+var cats=[
+    {name: "Xuxa", image: "images/xuxa_cat.png", click_count: 0},
+    {name: "Chewie", image: "images/chewie_cat.png", click_count: 0}
+];
 
+var catListElement = document.getElementById("cat-list");
+
+for (cat in cats) {
+    var catLINode = document.createElement("LI");
+    var catNameNode = document.createTextNode(cats[cat].name)
+    catLINode.appendChild(catNameNode);
+    document.getElementById("cat-list").appendChild(catLINode);
+};
+
+/*
 elem0.addEventListener('click', function(){
     click_count0++;
     document.getElementById("clicks0").innerHTML = click_count0;
@@ -12,3 +22,4 @@ elem1.addEventListener('click', function(){
     click_count1++;
     document.getElementById("clicks1").innerHTML = click_count1;
 }, false);
+*/
